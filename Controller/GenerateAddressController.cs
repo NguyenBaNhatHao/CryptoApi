e015449b4a15db0ee0523e92f2302042e635c11d
NguyenBaNhatHao <jdjjdjjdjdjdjddd@gmail.com>
2022-12-06 17:01:18 +0700

    update sql

--- Controller/GenerateAddressController.cs
+++ Controller/GenerateAddressController.cs
@@ -10,6 +10,7 @@ using System.Net.Http.Headers;
 using CryptoApi.Models;
 using System.Collections.Generic;
 using CryptoApi.Dtos;
+using CryptoApi.Datas;
 using Newtonsoft.Json;
 using DotNetEnv;
 
@@ -17,11 +18,12 @@ namespace CryptoApi.Controller{
     [Route("api/[controller]")]
     [ApiController]
     public class GenerateAddressController : ControllerBase{
-
+        public readonly CryptoApiDbContext _context;
         public readonly HttpClient _http;
 
-        public GenerateAddressController(HttpClient http){
+        public GenerateAddressController(HttpClient http, CryptoApiDbContext context){
             _http = http;
+            _context = context;
         }
 
         [HttpPost]
@@ -30,17 +32,22 @@ namespace CryptoApi.Controller{
                 cfg.AddProfile(new CrytoApiProfile());
             });
             var mapper = config.CreateMapper();
-
             Env.Load();
+            
             var ApiKey = Environment.GetEnvironmentVariable("ApiKey");
             var addressDTO = mapper.Map<Address,AddressDTO>(address);
-            
             string url = "https://rest.cryptoapis.io/wallet-as-a-service/wallets/"+addressDTO.walletid+"/"+addressDTO.blockchain+"/"+addressDTO.network+"/addresses";
             _http.DefaultRequestHeaders.Add("X-API-Key",ApiKey);
             string data = JsonConvert.SerializeObject(addressDTO);
             HttpContent c = new StringContent(data, Encoding.UTF8, "application/json");
             HttpResponseMessage httpResponse = _http.PostAsync(url, c).GetAwaiter().GetResult();
             httpResponse.EnsureSuccessStatusCode(); // throws if not 200-299
+            var sc = HttpStatusCode.OK;
+            if(httpResponse.StatusCode == sc){
+                address.label = addressDTO.data.item.label;
+                _context.AddressCrypto.Add(address);
+                _context.SaveChanges();
+            }
             var responseString = await httpResponse.Content.ReadAsStringAsync();
             Console.WriteLine(responseString);
             return Ok(responseString);                          
@@ -60,6 +67,13 @@ namespace CryptoApi.Controller{
             HttpContent c = new StringContent(data, Encoding.UTF8, "application/json");
             HttpResponseMessage httpResponse = _http.PostAsync(url, c).GetAwaiter().GetResult();
             httpResponse.EnsureSuccessStatusCode(); // throws if not 200-299
+            if((int)httpResponse.StatusCode == 201){
+                sendCoin.amount = sendCoinDto.data.item.amount;
+                sendCoin.note = sendCoinDto.data.item.note;
+                sendCoin.recipientAddress = sendCoinDto.data.item.recipientAddress;
+                _context.TransactionRequest.Add(sendCoin);
+                _context.SaveChanges();
+            }
             var responseString = await httpResponse.Content.ReadAsStringAsync();
             Console.WriteLine(responseString);
             return Ok(responseString);              
